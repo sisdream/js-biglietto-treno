dcf3cce20c9f5b027c9c172dabd0c9baecdc7eb0
sisdream <sissipascale2@gmail.com>
2024-01-09 15:21:19 +0100

    feat: sconti

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ console.log(path);
 
 let age = prompt("Quanti anni hai?");
 console.log(age);
+
+// Costo del biglietto
+
+let costo = document.getElementById(".costo");
+const costoPerKm = 0.21;
+
+// Biglietto
+
+let tiket = path * costoPerKm;
+console.log(tiket);
+
+// Sconti 
+
+//Sconto per minori 20%
+let scontoMinori = tiket / 100 * 20;
+console.log(scontoMinori);
+//Sconto per anziani 40%
+let scontoAnziani = tiket / 100 * 40;
+console.log(scontoAnziani);
